a7e823b39518b249d0e5d0d00adda4fac2307307
mr_yussan <yussandeveloper@gmail.com>
2014-08-10 10:59:11 +0700

    edit conection

--- conection.php
+++ conection.php
@@ -1,7 +1,7 @@
 <?php
     $dbhost = "localhost";
     $dbuser = "root";
-    $dbpass = "rahasia";
+    $dbpass = "";
     $dbnama = "db_myrewriter";
     $dbcon = mysql_connect($dbhost, $dbuser, $dbpass) or die("Database Error");
     $tesdb = mysql_select_db($dbnama, $dbcon) or die ("database $dbnama tidak bisa diakses");
